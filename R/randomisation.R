5b27abced72e085b6e36feb627c622331be65c07
Fonti Kar <fonti.kar@gmail.com>
2016-11-27 09:51:03 +1100

    code to randomise males to incubators and chambers across 10 periods

--- R/randomisation.R
+++ R/randomisation.R
@@ -1,21 +1,23 @@
 setwd("~/Dropbox/smrrxn/")
 
 maledat <- read.csv("data/LDMales_alive.csv")
+names(maledat)
+
 
 #Constructing randomisation dataframe for male lizards
 
-randat <- data.frame(period = rep(c(1:10), each = 45),
-                     batch = rep(c(1:2), times = c(23, 22)),
-                     incubator = rep(c(1,2,1,2), times = c(11,12,11,11)),
-                     chamber = rep(c(1:23, 1:22), times = 10))
+randat <- data.frame(period = rep(c(1:10), each = 42),
+                     batch = rep(c(1:2), times = c(21, 21)),
+                     incubator = rep(c(1,2,1,2), times = c(10,11,10,11)),
+                     chamber = rep(c(1:21, 1:21), times = c(10)))
 
-randat <- randat[order(randat$batch),]                              
+randat <- randat[order(randat$batch),]    
+str(randat)
 
 #Assigning males to B1 and B2
 
-ids <- sample(maledat$id, replace = F)
-b1_ids <- ids[1:23]
-b2_ids <- ids[24:45]
+b1_ids <- maledat$id[1:21]
+b2_ids <- maledat$id[22:42]
 
 #Sampling B1 ids for 10 periods
 allb1 <- replicate(10,sample(b1_ids, replace = F))
@@ -26,29 +28,22 @@ allb2 <- unlist(list(allb2))
 
 randat$id <- c(allb1, allb2)
 
-
-
-###########
-ids <- replicate(10,sample(maledat$id, replace = F))
-ids_all <- unlist(list(ids))
-
-randat$id <- ids_all
-
-table(randat[randat$period == "10",5])
+unique(randat[randat$batch == 1, 5]) 
+unique(randat[randat$batch == 2, 5])
 
 #merge randat and maledat together
-str(randat)
+
 randat$id <- as.factor(randat$id)
 
 str(maledat)
 
-final_randat <- merge(randat, maledat[,1:3])
+final_randat <- merge(randat, maledat[,5:7])
 
-final_randat <- final_randat[c(2,3,4,5,1,6,7)]
+final_randat<- final_randat[order(final_randat$period, final_randat$batch, final_randat$incubator, final_randat$chamber),]
 
-final_randat<- final_randat[order(final_randat$period),]
+final_randat <- final_randat[c(2,3,4,5,1,7,6)]
 
-write.csv(final_randat, file = "data/randomisation.csv", row.names = F) 
+write.csv(final_randat, file = "data/randomisation_v3.csv", row.names = F) 
 
 
 #Constructing randomisation dataframe for temperatures to incbuators
@@ -60,7 +55,7 @@ tempdat <- data.frame(period = rep(c(1:10), each = 24),
 
 tempdat <- tempdat[order(tempdat$incubator),]
 
-write.csv(tempdat, file = "data/temprandom.csv", row.names = F)
+
 
 #day 3 temp
 replicate(20, sample(c(30,32)))
@@ -71,3 +66,9 @@ day_2 <- replicate(20, sample(c(26,28)))
 #day 1 temp
 day_1 <-replicate(20, sample(c(22,24)))
 
+#randomise temps
+tempdat$temp <- unlist(list(replicate(40, sample(c(22,24,26,28,30,32), replace =F))))
+
+write.csv(tempdat, file = "data/temprandom_V2.csv", row.names = F)
+
+getwd()
