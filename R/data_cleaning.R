1e23dd3e709a791ecdd316863ba1c4e72b43c22a
Fonti Kar <fonti.kar@gmail.com>
2017-07-12 16:40:34 +1000

    Code for finding outliers and indicating them in dataset, and code for running models in lme4

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -43,7 +43,7 @@ pop.data <- pop.data[,c(1,4)]
 names(pop.data) <- c("id", "site")
 
 newdata <- merge(finaldata, pop.data)
-str(newdata) #2541 obs
+str(newdata) #2520 obs
 
 #obs variable
 
@@ -68,14 +68,14 @@ ggplot(newdata, aes(lizmass, obs, label = obs)) +
 
 #Potential outliers for mass:  359, 377  - typos in ch_mass
 
-newdata[newdata$obs == 359, "ch_mass"] <- 18.195
-newdata[newdata$obs == 377, "ch_mass"] <-  17.824 
+newdata[newdata$obs == 379, "ch_mass"] <- 18.195
+newdata[newdata$obs == 356, "ch_mass"] <-  17.824 
 
 newdata$lizmass <- newdata$ch_lizmass - newdata$ch_mass
 
-#Potential outliers for mass >2 : 1649 1654 1997 2003 2441 2456 2461 2477 #4 rows of data
+#Potential outliers for mass >2 : 1641 1675 1983 1993 2421 2429 2434 2453
 newdata[newdata$lizmass > 2,"obs"] 
-great2mass.obs <- c(1649, 1654, 1997, 2003, 2441, 2456, 2461, 2477)
+great2mass.obs <- c(1641, 1675, 1983, 1993, 2421, 2429, 2434, 2453)
 newdata[newdata$obs %in% great2mass.obs,]
 
 #obs 1649 1654 not a typo but inconsisent with ID mean mass
@@ -83,70 +83,73 @@ ld0128 <- newdata[newdata$id == "ld0128",]
 ld0128[,"lizmass"]
 summary(subset(ld0128, lizmass < 2, select = lizmass))
 #Putting mean of lizard in place of error? or NA?
-newdata[newdata$id == "ld0128" & newdata$obs == c(1649, 1654), "lizmass"] #<- rep(1.137,2)
+ld0128[ld0128$obs == 1641, "lizmass"] #<- rep(1.137,2)
+ld0128[ld0128$obs == 1675, "lizmass"] #<- rep(1.137,2)
 
-#obs 1997, 2003 ch_mass typo -  need to recalculate liz mass after fixing these ch_mass errors
-newdata[newdata$id == "ld0143" & newdata$obs == 1997, "ch_mass"] <- 18.142
-newdata[newdata$id == "ld0143" & newdata$obs == 2003, "ch_mass"] <- 18.142
+#obs 1983 1993 ch_mass typo -  need to recalculate liz mass after fixing these ch_mass errors
+newdata[newdata$id == "ld0143" & newdata$obs == 1983, "ch_mass"] <- 18.142
+newdata[newdata$id == "ld0143" & newdata$obs == 1993, "ch_mass"] <- 18.142
 
-#obs 2441 2456 ch_mass typo - need to recalculate liz mass after fixing these ch_mass errors
-newdata[newdata$id == "ld0171" & newdata$obs == 2441, "ch_mass"] <- 18.104
-newdata[newdata$id == "ld0171" & newdata$obs == 2456, "ch_mass"] <- 18.104
+#obs 2429 2434 ch_mass typo - need to recalculate liz mass after fixing these ch_mass errors
+newdata[newdata$id == "ld0171" & newdata$obs == 2434, "ch_mass"] <- 18.104
+newdata[newdata$id == "ld0171" & newdata$obs == 2429, "ch_mass"] <- 18.104
 
-#obs 2461, 2477 not a typo but inconsisent with ID mean mass *see above, had issues with this lizard's ch_mass too
+#obs 2421, 2453 not a typo but inconsisent with ID mean mass *see above, had issues with this lizard's ch_mass too
 ld0171  <- newdata[newdata$id == "ld0171",]
 ld0171[,"lizmass"]
 summary(subset(ld0171, lizmass < 2, select = lizmass))
 #Putting mean of lizard in place of error? or NA?
-newdata[newdata$id == "ld0171" & newdata$obs == c(2461, 2477), "lizmass"] #<- rep(1.170,2)
+ld0171[ld0171$obs == 2421, "lizmass"] #<- rep(1.137,2)
+ld0171[ld0171$obs == 2453, "lizmass"] #<- rep(1.137,2)
 
-#Potential outliers for mass <0.5 : 77  117  146  166 560  563 1306 1327 1457 1496 1601 1623 2134 2176 2287 2288 2395 2403 #9 rows of data
+#Potential outliers for mass <0.5 : 76  112  147  167 560  573 1286 1312 1445 1484 1588 1614 2130 2155 2241 2272 2377 2383
 newdata[newdata$lizmass < 0.5,"obs"] 
-less0.5mass.obs <- c(77, 117, 146, 166, 560, 563, 1306, 1327, 1457, 1496, 1601, 1623, 2134, 2176, 2287, 2288, 2395, 2403)
+less0.5mass.obs <- c(76, 112, 147, 167, 560, 573, 1286, 1312, 1445, 1484, 1588, 1614, 2130, 2155, 2241, 2272, 2377, 2383)
 newdata[newdata$obs %in% less0.5mass.obs,]
 
-#obs 77, 117 must be a typo, inconsisent with ID mean mass 
+#obs 76, 112 must be a typo, inconsisent with ID mean mass 
 ld0006   <- newdata[newdata$id == "ld0006",]
-ld0006 [,"lizmass"]
+ld0006[,"lizmass"]
 summary(subset(ld0006, lizmass > 0.5, select = lizmass))
 #Putting mean of lizard in place of error? or NA?
-newdata[newdata$id == "ld0171" & newdata$obs == c(2461, 2477), "lizmass"] #<- rep(1.035,2)
+ld0006[ld0006$obs == 76, "lizmass"] #<- rep(1.042,2)
+ld0006[ld0006$obs == 112, "lizmass"] #<- rep(1.042,2)
 
-#obs 146, 166 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0011" & newdata$obs == 146, "ch_lizmass"] <- 19.008
-newdata[newdata$id == "ld0011" & newdata$obs == 166, "ch_lizmass"] <- 19.008
+#obs 147  167 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0011" & newdata$obs == 147, "ch_lizmass"] <- 19.008
+newdata[newdata$id == "ld0011" & newdata$obs == 167, "ch_lizmass"] <- 19.008
 
-#obs 560, 563 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+#obs 560, 573 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
 newdata[newdata$id == "ld0049" & newdata$obs == 560, "ch_lizmass"] <- 19.208
-newdata[newdata$id == "ld0049" & newdata$obs == 563, "ch_lizmass"] <- 19.208
+newdata[newdata$id == "ld0049" & newdata$obs == 573, "ch_lizmass"] <- 19.208
 
-#obs 1306, 1327 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0105" & newdata$obs == 1306, "ch_lizmass"] <- 19.451
-newdata[newdata$id == "ld0105" & newdata$obs == 1327, "ch_lizmass"] <- 19.451
+#obs 1286, 1312 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0105" & newdata$obs == 1286, "ch_lizmass"] <- 19.451
+newdata[newdata$id == "ld0105" & newdata$obs == 1312, "ch_lizmass"] <- 19.451
 
-#obs 1457, 1496 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0115" & newdata$obs == 1457, "ch_lizmass"] <- 19.589
-newdata[newdata$id == "ld0115" & newdata$obs == 1496, "ch_lizmass"] <- 19.589
+#obs 1445, 1484 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0115" & newdata$obs == 1445, "ch_lizmass"] <- 19.589
+newdata[newdata$id == "ld0115" & newdata$obs == 1484, "ch_lizmass"] <- 19.589
 
-#obs 1601, 1623 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0119" & newdata$obs == 1601, "ch_lizmass"] <- 19.512
-newdata[newdata$id == "ld0119" & newdata$obs == 1623, "ch_lizmass"] <- 19.512
+#obs 1588, 1614 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0119" & newdata$obs == 1588, "ch_lizmass"] <- 19.512
+newdata[newdata$id == "ld0119" & newdata$obs == 1614, "ch_lizmass"] <- 19.512
 
-#obs 2134, 2176 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0150" & newdata$obs == 2134, "ch_lizmass"] <- 19.033
-newdata[newdata$id == "ld0150" & newdata$obs == 2176, "ch_lizmass"] <- 19.033
+#obs 2130, 2155 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0150" & newdata$obs == 2130, "ch_lizmass"] <- 19.033
+newdata[newdata$id == "ld0150" & newdata$obs == 2155, "ch_lizmass"] <- 19.033
 
-#obs 2287 2288 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0154" & newdata$obs == 2287, "ch_lizmass"] <- 19.840
-newdata[newdata$id == "ld0154" & newdata$obs == 2288, "ch_lizmass"] <- 19.840
+#obs  2241, 2272 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0154" & newdata$obs == 2241, "ch_lizmass"] <- 19.840
+newdata[newdata$id == "ld0154" & newdata$obs == 2272, "ch_lizmass"] <- 19.840
 
-#obs 2395 2403 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
-newdata[newdata$id == "ld0170" & newdata$obs == 2395, "ch_lizmass"] <- 19.504
-newdata[newdata$id == "ld0170" & newdata$obs == 2403, "ch_lizmass"] <- 19.504
+#obs 2377, 2383 ch_lizmass typo  - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0170" & newdata$obs == 2377, "ch_lizmass"] <- 19.504
+newdata[newdata$id == "ld0170" & newdata$obs == 2383, "ch_lizmass"] <- 19.504
 
-#Potential outliers for  mass > 1.5 and < 2 : 310  362  365  415 1523 1537 #3 rows of data
-newdata[newdata$lizmass > 1.5 & newdata$lizmass < 2,"obs"] 
-less2great1.5.obs <- c(310, 362, 365, 415, 1523, 1537)
+#Potential outliers for  mass > 1.6 and < 2 : 342  350  414  417 1519 1547 
+newdata[newdata$lizmass > 1.6 & newdata$lizmass < 2,"obs"] 
+less2great1.5.obs <- c(340, 350, 414, 417, 1519, 1547)
 newdata[newdata$obs %in% less2great1.5.obs,]
 
 #obs 310, 362 - not a typo but inconsisent with ID mean mass
@@ -173,15 +176,21 @@ summary(subset(ld0118, lizmass < 1.7 , select = lizmass))
 newdata[newdata$id == "ld0118" & newdata$obs == 1523, "lizmass"] #<- 1.326 
 newdata[newdata$id == "ld0118" & newdata$obs == 1537, "lizmass"] #<- 1.326 
 
-#Potential outliers for  mass > 0.5 and < 0.8 :  83   96  #1 rows of data
-newdata[newdata$lizmass > 0.5 & newdata$lizmass < 0.8,"obs"] 
-newdata[newdata$obs == c(83,96),]
+#Potential outliers for  mass > 0.5 and < 0.7 :  78   83
+newdata[newdata$lizmass > 0.5 & newdata$lizmass < 0.80,"obs"] 
+newdata[newdata$obs %in% c(78, 83) ,]
 
 #obs  83 96 ch_lizmass typo - need to recalculate liz mass after fixing these ch_lizmass errors
+newdata[newdata$id == "ld0006" & newdata$obs == 78, "ch_lizmass"] <- 19.537
 newdata[newdata$id == "ld0006" & newdata$obs == 83, "ch_lizmass"] <- 19.537
-newdata[newdata$id == "ld0006" & newdata$obs == 96, "lizmass"] <- 19.537
 
 #newdata$lizmass <- newdata$ch_lizmass - newdata$ch_mass
+#Combining all true mass outliers together
+
+truemassout.obs <- c(1641, 1675, 2421, 2453, 76, 112, 310, 362, 365, 415, 1523, 1537)
+truemassout <- newdata[newdata$obs %in% truemassout.obs,]
+
+newdata$mass_outlier <- ifelse(newdata$obs %in% truemassout.obs, 1,  0) 
 
 ###cleveland dot plot for co2: 
 ggplot(newdata,
@@ -194,12 +203,21 @@ ggplot(newdata, aes(co2_pmin, obs, label = obs)) +
   geom_text() + 
   theme_bw()
 
-#Potential outliers for co2 > 0.02: 24   25   32  139  244  252  288  295  432  621  660  664  770  773  985 1006 1012 1022 1070 1131 1133 1213 1289 1297 1454 1483 1502 1522 1762 2356 #30 rows of data using obs 432 as cut off
-newdata[newdata$obs == 432,"co2_pmin"]
+#Potential outliers for co2 > 0.02328337 (obs 2304 as cutoff): 249  766 1015 1298
+newdata[newdata$obs == 2304, "co2_pmin"]
 vec <- newdata[newdata$co2_pmin > 0.02 ,"obs"] 
 great0.02co2.obs <- vec[!is.na(vec)]
-great0.02co2.obs.dat <- newdata[newdata$obs %in% great0.02co2.obs, ]
+co2outs <- newdata[newdata$obs %in% great0.02co2.obs, ]
+write.csv(co2outs , row.names = F, "data/co2outliers.csv")
 
+#Treat the ones with notes in aircollect, fms and warthog as outliers as they were mechanic errors 
+
+co2trueouts.obs <- c(979, 1053, 1015, 262, 1273, 274, 633, 1122, 1443, 1298 ,249 ,766, 1230, 645, 1471, 1490, 994, 2304, 1530, 14)
+co2trueouts <- newdata[newdata$obs %in% co2trueouts.obs, ]
+co2trueouts[order(co2trueouts$obs),]
+
+newdata$co2_outlier <- ifelse(newdata$obs %in% co2trueouts.obs, 1,  0) 
+ 
 ###cleveland dot plot for incb_temp: #all is fine
 
 ggplot(newdata, aes(incb_temp, obs, label = obs)) +
@@ -217,72 +235,39 @@ ggplot(newdata, aes(body_temp, obs, label = obs)) +
 #Outlier in body temp
 newdata[newdata$body_temp <10,'obs']
 
-#obs 1394, 1399 TYPO
-newdata[newdata$obs == 1394,"body_temp"] <- 24
-newdata[newdata$obs == 1399,"body_temp"] <- 24 
+#obs 1387 1390 TYPO
+newdata[newdata$obs == 1387,"body_temp"] <- 24
+newdata[newdata$obs == 1390,"body_temp"] <- 24 
 
 newdata[newdata$body_temp >31,'obs']
 
-#obs 1881 1918 #This is not a typo it was what was recorded
-newdata[newdata$obs == 1881,] 
-newdata[newdata$obs == 1918,] 
+#obs 1881 1918 #This is not a typo it was what was recorded - decide to leave this
+newdata[newdata$obs == 1864,] 
+newdata[newdata$obs == 1892,] 
 
 newdata[newdata$id == "ld0140","body_temp"]
-
-newdata[newdata$obs == 1881,"body_temp"] 
-newdata[newdata$obs == 1918,"body_temp"] 
 
 #Scatterplot of co2~mass with obs labels
 #Outliers 
 
 plot(log(newdata$co2_pmin) ~ log(newdata$lizmass))
 
-ggplot(data, aes(y = log(co2_pmin), x = log(lizmass), label = obs)) +
-  geom_point(colour = "white") +
-  geom_text() + 
-  #scale_x_continuous(limits = c(-4, 3)) + 
-  #scale_y_continuous(limits = c(-6, 3)) +
-  theme_bw()
+#Potential outliers: co2~mass obs less than -7.5 on log co2 scale : 1348, 1836
+less7.5logco2mass <- newdata[log(newdata$co2_pmin) < -7.5,"obs"]
+less7.5logco2mass.obs <- less7.5logco2mass[!is.na(less7.5logco2mass)]
 
-#Potential outliers: co2~mass obs less than -6.6 on log co2 scale : 113  160  168  242  326  388  401  801 1360 1638 1827 1862 1986 2094 2296 2384
-less6.6logco2mass <- newdata[log(newdata$co2_pmin) < -6.6,"obs"]
-less6.6logco2mass.obs <- less6.6logco2mass[!is.na(less6.6logco2mass)]
+#Potential outliers: co2~mass obs less than -0.5 on log mass scale :  76 112
+less0.5logco2mass <- newdata[log(newdata$lizmass) < -0.5,'obs']
+less0.5logco2mass.obs <- less0.5logco2mass[!is.na(less0.5logco2mass)]
 
-#Potential outliers: co2~mass obs less than -0.8 on log mass scale :  77  117  146  166  560  563 1306 1327 1457 1496 1601 1623 2134 2176 2287 2288 2395 2403
-less1logco2mass <- newdata[log(newdata$lizmass) < -0.8,"obs"]
-less1logco2mass.obs <- less1logco2mass[!is.na(less1logco2mass)]
+#Potential outliers: co2~mass obs greater than 0.5 on log mass scale :  414  417 1519 1547 1641 1675 2421 2453
+great0.5logco2mass <- newdata[log(newdata$lizmass) > 0.5,'obs']
+great0.5logco2mass.obs <- great0.5logco2mass[!is.na(great0.5logco2mass)]
 
-#Recalcuation of key variables
-newdata$lizmass <- newdata$ch_lizmass - newdata$ch_mass
-newdata$total_air <- newdata$ch_vol - newdata$lizmass
-newdata$total_co2 <- newdata$frac_co2 * newdata$total_air
-newdata$co2_pmin <- newdata$total_co2 / newdata$t_diff
+co2massout <- c(less7.5logco2mass.obs, less0.5logco2mass.obs, great0.5logco2mass)
+co2massout.obs <- co2massout[!is.na(co2massout)]
 
-#Write newdata as csv
-
-write.csv(newdata,row.names = F, "data/data_final/mrrxn_nooutlier.csv")
-
-#standardising and transforming variables
-
-data$z.incb_temp <- scale(data$incb_temp)
-data$z.log.temp <- scale(log(data$incb_temp))
-
-data$z.body_temp <- scale(data$body_temp)
-data$z.log.body_temp <- scale(log(data$body_temp))
-
-data$log.mass <- log(data$lizmass)
-data$z.log.mass <- scale(data$log.mass)
-
-data$log.co2pmin <- log(data$co2_pmin)
-data$z.log.co2pmin <- scale(data$log.co2pmin)
-
-#Standardising and transforming prior temp variables 
-
-data$z.prior_temp_3 <- scale(data$prior_temp_3)
-data$z.log.prior_temp_3 <- scale(log(data$prior_temp_3))
-
-data$z.prior_temp_4 <- scale(data$prior_temp_4)
-data$z.log.prior_temp_4 <- scale(log(data$prior_temp_4))
+newdata$co2mass_outlier <- ifelse(newdata$obs %in% co2massout.obs, 1,  0)
 
 #Boltzman standardisation of temperature predictors
 #myTemperatureInKelvin  <-  1:30 + 273.15
@@ -290,12 +275,64 @@ data$z.log.prior_temp_4 <- scale(log(data$prior_temp_4))
 #where 8.62e-5 is the Boltzmann constant (in eV/K). this transformation ensures that your intercept is also independent of temperature (and not only mass), and corresponds to the metabolic rate at mean(myTemperatureInKelvin).
 
 #incb_temp
-data$incb_temp_K <- data$incb_temp + 273.15
-data$inverseK_incb_temp <- 1 / 8.62e-5 * ((1 / mean(data$incb_temp_K)) - (1 /data$incb_temp_K))
+newdata$incb_temp_K <- newdata$incb_temp + 273.15
+newdata$inverseK_incb_temp <- 1 / 8.62e-5 * ((1 / mean(newdata$incb_temp_K)) - (1 /newdata$incb_temp_K))
 
 #body_temp
-data$body_temp_K <- data$body_temp + 273.15
-data$inverseK_body_temp <- 1 / 8.62e-5 * ((1 / mean(data$body_temp_K)) - (1 /data$body_temp_K))
+newdata$body_temp_K <- newdata$body_temp + 273.15
+newdata$inverseK_body_temp <- 1 / 8.62e-5 * ((1 / mean(newdata$body_temp_K, na.rm =T)) - (1 /newdata$body_temp_K))
 
-#prior_temp3, 4
+#Write newdata as csv
 
+write.csv(newdata,row.names = F, "data/data_final/mrrxn_orig.csv")
+
+#Read in data again after putting NA in mass and co2 if allcomb_outlier indicator = 1 and creating prior_temp1 and prior_temp2
+newdata <- read.csv("data/data_final/mrrxn_orig.csv")
+str(newdata) #2520 obs
+
+#Recalcuation of key variables
+newdata$lizmass <- newdata$ch_lizmass - newdata$ch_mass
+newdata$total_air <- newdata$ch_vol - newdata$lizmass
+newdata$total_co2 <- newdata$frac_co2 * newdata$total_air
+newdata$co2_pmin <- newdata$total_co2 / newdata$t_diff
+
+#standardising and transforming variables
+
+newdata$z.incb_temp <- scale(newdata$incb_temp)
+newdata$z.log.temp <- scale(log(newdata$incb_temp))
+
+newdata$z.body_temp <- scale(newdata$body_temp)
+newdata$z.log.body_temp <- scale(log(newdata$body_temp))
+
+newdata$log.mass <- log(newdata$lizmass_nocombout)
+newdata$z.log.mass <- scale(newdata$log.mass)
+
+newdata$log.co2pmin <- log(newdata$co2pm_nocombout)
+newdata$z.log.co2pmin <- scale(newdata$log.co2pmin)
+
+#Standardising and temp correctiong prior temp variables
+
+#incb_temp
+newdata$incb_temp_K <- newdata$incb_temp + 273.15
+newdata$inverseK_incb_temp <- 1 / 8.62e-5 * ((1 / mean(newdata$incb_temp_K)) - (1 /newdata$incb_temp_K))
+
+#body_temp
+newdata$body_temp_K <- newdata$body_temp + 273.15
+newdata$inverseK_body_temp <- 1 / 8.62e-5 * ((1 / mean(newdata$body_temp_K, na.rm =T)) - (1 /newdata$body_temp_K))
+
+newdata$z.prior_temp1 <- scale(newdata$prior_temp1)
+newdata$z.log.prior_temp1 <- scale(log(newdata$prior_temp1))
+
+newdata$z.prior_temp2 <- scale(newdata$prior_temp2)
+newdata$z.log.prior_temp2 <- scale(log(newdata$prior_temp2))
+
+#prior_temp 1
+newdata$prior_temp1_K <- newdata$prior_temp1 + 273.15
+newdata$inverseK_prior_temp1 <- 1 / 8.62e-5 * ((1 / mean(newdata$prior_temp1_K, na.rm =T)) - (1 /newdata$prior_temp1_K))
+
+#prior_temp 2
+newdata$prior_temp2_K <- newdata$prior_temp2 + 273.15
+newdata$inverseK_prior_temp2 <- 1 / 8.62e-5 * ((1 / mean(newdata$prior_temp2_K, na.rm =T)) - (1 /newdata$prior_temp2_K))
+
+View(newdata)
+write.csv(newdata, row.names = F, "data/data_final/mrrxn_final.csv")
--- R/lme4_analysis.R
+++ R/lme4_analysis.R
@@ -7,15 +7,38 @@ library(lme4)
 
 #histograms
 
-data <- read.csv("data/data_final/mr_final_analysis.csv")
+data <- read.csv("data/data_final/mrrxn_final.csv")
+str(data)
+names(data)
+
+varibs.excl <- c("tc", "date", "ch_num", "key", "ch_mass", "ch_lizmass", "t_flush", "t_samp", "t_diff", "ch_vol", "co2_samp_control", "co2_samp_1", "co2_samp_2", "air_collect_notes", "fms_notes", "co2_warthog_notes", "co2_samp_1_correct", "co2_samp_2_correct", "frac_co2", "total_air", "total_co2")
+
+varibs.need <- c("obs", "samp_period", "id" ,"batch", "series", "incb_num", "incb_temp_id", "defecate", "incb_temp", "z.incb_temp", "z.log.temp", "incb_temp_K", "inverseK_incb_temp", "body_temp", "z.body_temp", "z.log.body_temp", "body_temp_K", "inverseK_body_temp" , "z.prior_temp1", "z.log.prior_temp1", "prior_temp1_K", "inverseK_prior_temp1", "prior_temp2_K", "inverseK_prior_temp2", "z.prior_temp2", "z.log.prior_temp2", "orig_lizmass", "lizmass_nocombout", "log.mass", "z.log.mass","orig_co2_pmin", "co2pm_nocombout", "log.co2pmin", "z.log.co2pmin")
+
+incl.vars <- names(data) %in% varibs.need
+data <- data[incl.vars]
+View(data)
+str(data)
 
 #Normality
-
+#Temp predictors
 hist(data$z.incb_temp) ; shapiro.test(data$z.incb_temp)
-hist(log(data$incb_temp)) ; shapiro.test(log(data$incb_temp))
+hist(data$z.log.temp) ; shapiro.test(data$z.log.temp)
+hist(data$inverseK_incb_temp) ; shapiro.test(data$inverseK_incb_temp)
 
 hist(data$z.body_temp) ; shapiro.test(data$z.body_temp)
+hist(data$z.log.prior_temp1). ; shapiro.test(data$z.log.prior_temp1)
+hist(data$inverseK_body_temp) ; shapiro.test(data$inverseK_body_temp)
 
+hist(data$z.prior_temp1) ; shapiro.test(data$z.prior_temp1)
+hist(data$z.log.prior_temp1) ; shapiro.test(data$z.log.prior_temp1)
+hist(data$inverseK_prior_temp1) ; shapiro.test(data$inverseK_prior_temp1)
+
+hist(data$z.prior_temp2) ; shapiro.test(data$z.prior_temp2)
+hist(data$z.log.prior_temp2) ; shapiro.test(data$z.log.prior_temp2)
+hist(data$inverseK_prior_temp2) ; shapiro.test(data$inverseK_prior_temp2)
+
+#Mass predictors
 hist(data$log.mass) ; shapiro.test(data$log.mass)
 hist(data$z.log.mass) ; shapiro.test(data$z.log.mass)
 
@@ -24,79 +47,83 @@ hist(data$z.log.co2pmin) ; shapiro.test(data$z.log.co2pmin)
 
 #batch and incubation number and defecation effects
 #on mr
-boxplot(data$z.log.co2pmin ~ data$site)
 boxplot(data$z.log.co2pmin ~ data$batch)
 boxplot(data$z.log.co2pmin ~ data$incb_num)
+
+data$defecate <- ifelse(is.na(data$defecate), 0,1) 
 boxplot(data$z.log.co2pmin ~ data$defecate)
 
 #on mass
-boxplot(data$z.log.mass ~ data$site) #Quite diff in mass between pops
 boxplot(data$z.log.mass ~ data$batch)
 boxplot(data$z.log.mass ~ data$incb_num)
 boxplot(data$z.log.mass ~ data$defecate)
 
 #plotting to see if there is non-linear relationship
-boxplot(data$co2_pmin ~ data$incb_temp) #Doesn't look like it
+plot(data$z.log.co2pmin  ~ data$inverseK_incb_temp) #Doesn't look like it
 
-plot(log(data$co2_pmin) ~ log(data$lizmass)) #Doesn't look like it
+plot(data$z.log.co2pmin  ~ data$z.log.mass) #Doesn't look like it
 
-#Carryover effects of temperature order
-model.1.1 <- lmer(z.log.co2pmin ~ z.incb_temp + z.log.mass + z.body_temp + (1+z.incb_temp|id) + (1+z.incb_temp|series), data = data)
-summary(model.1.1)
-AIC(model.1.1) #5341.076
-
-model.1.2 <- lmer(z.log.co2pmin ~ z.incb_temp + z.log.mass + z.prior_temp_3 + (1+z.incb_temp|id) + (1+z.incb_temp|series), data = data)
-summary(model.1.2)
-AIC(model.1.2) #5345.327
-
-plot(data$co2_pmin ~ data$z.prior_temp_3)
-plot(data$co2_pmin ~ data$z.log.prior_temp_3)
-
-model.1.3 <- lmer(z.log.co2pmin ~ z.incb_temp + z.log.mass + z.log.prior_temp_4 + (1+z.incb_temp|id) + (1+z.incb_temp|series), data = data)
-summary(model.1.3)
-AIC(model.1.3) #5278.598
-
-plot(data$co2_pmin ~ data$z.prior_temp_4)
-plot(data$co2_pmin ~ data$z.log.prior_temp_4)
+ggplot(data, aes(x = z.log.mass, y = z.log.co2pmin, label = obs)) +
+  geom_point(colour = "white") +
+  geom_text() + 
+  theme_bw() 
 
 #Collinearity
 names(data)
-varibs <- c("z.log.co2pmin", "incb_temp", "z.log.mass", "z.prior_temp_3", "z.prior_temp_4")
+varibs <- c("z.log.co2pmin", "inverseK_incb_temp", "z.log.mass", "inverseK_prior_temp1", "inverseK_prior_temp2")
 cor(data[,varibs],use = "complete.obs") 
 
-pairs(data[,c(varibs)], log = "xy")
+pairs(data[,c(varibs)])
 
-#Analysis
-#To use log temp or not
-model.2 <- lmer(z.log.co2pmin ~ z.incb_temp + z.log.mass + z.prior_temp_4 + (1+z.incb_temp|id) + (1+z.incb_temp|series), data = data)
-summary(model.2)
-AIC(model.2) #5278.598
-plot(model.2)
-qqnorm(resid(model.2))
+#Use inverse temp or z-transformed
+model.1.1 <- lmer(z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + (1+inverseK_incb_temp|id) + (1+inverseK_incb_temp|series), data = data)
+summary(model.1.1)
+AIC(model.1.1) #5268.247
 
-model.2.2 <- lmer(z.log.co2pmin ~ z.log.temp + z.log.mass + z.prior_temp_4 + (1+z.log.temp|id) + (1+z.log.temp|series), data = data)
+model.1.2 <- lmer(z.log.co2pmin ~ z.incb_temp + z.log.mass + (1+z.incb_temp|id) + (1+z.incb_temp|series), data = data)
+summary(model.1.2)
+AIC(model.1.2) #5271.841
+
+model.1.3 <- lmer(z.log.co2pmin ~ z.log.temp + z.log.mass + (1+z.log.temp|id) + (1+z.log.temp|series), data = data)
+summary(model.1.3)
+AIC(model.1.3) #5266.752  #No AIC diff in z.log.temp and inverseK temp
+
+#Carry over effects of prior temps using inverse K Temp
+model.2.1 <- lmer(z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + inverseK_body_temp + (1+inverseK_incb_temp|id) + (1+inverseK_incb_temp|series), data = data)
+summary(model.2.1)
+AIC(model.2.1) #5205.507
+
+model.2.2 <- lmer(z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + inverseK_prior_temp1 + (1+inverseK_incb_temp|id) + (1+inverseK_incb_temp|series), data = data)
 summary(model.2.2)
-AIC(model.2.2) #5267.552 #Only a little better using log temp
+AIC(model.2.2) #5203.591
 plot(model.2.2)
 qqnorm(resid(model.2.2))
 
+model.2.3 <- lmer(z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + inverseK_prior_temp2 + (1+inverseK_incb_temp|id) + (1+inverseK_incb_temp|series), data = data)
+summary(model.2.3)
+AIC(model.2.3) #5145.008 #This is the best final model 
+plot(model.2.3)
+qqnorm(resid(model.2.3)) 
+
+#Analysis
+
 #R intercept
 #print(VarCorr(model.2.1),comp=c("Variance", "Std.Dev."))
-mod2.2 <- data.frame(VarCorr(model.2.2, comp = "Variance"))
+mod2.3 <- data.frame(VarCorr(model.2.3, comp = "Variance"))
 
-mod2.2[4,4] / (mod2.2[4,4] + mod2.2[1,4]) #0.494933
+mod2.3[4,4] / (mod2.3[4,4] + mod2.3[1,4]) #0.5185752
 
 #R slope
 
-mod2.2[5,4] / (mod2.2[5,4] + mod2.2[2,4]) #0.2683569
+mod2.3[5,4] / (mod2.3[5,4] + mod2.3[2,4]) #0.3543051
 
 #R short term
 
-(mod2.2[4,4] +  mod2.2[1,4]) / (mod2.2[4,4] +  mod2.2[1,4] + mod2.2[7,4])
+(mod2.3[4,4] +  mod2.3[1,4]) / (mod2.3[4,4] +  mod2.3[1,4] + mod2.3[7,4]) #0.3050962
 
 #R long term
 
-mod2.2[4,4] / (mod2.2[4,4] +  mod2.2[1,4] + mod2.2[7,4])
+mod2.3[4,4] / (mod2.3[4,4] +  mod2.3[1,4] + mod2.3[7,4]) #0.1582153
 
 
 
