0d1d059198c399001f7ae807ff26e871fdb7d855
Fonti Kar <fonti.kar@gmail.com>
2017-08-01 07:39:38 +1000

    Included model code from katana

--- R/mrrxn_MCMC.R
+++ R/mrrxn_MCMC.R
@@ -6,13 +6,39 @@ rm(list = ls())
 #load library
 library(MCMCglmm)
 
+# Define whether to run code
+
+m1 <- FALSE
+m2 <- FALSE
+m3 <- FALSE
+
+#priors
+expanded.prior <- list(R = list(V = 1, nu = 0.002),
+                       G = list(G1 = list(V = diag(2), nu = 0.002, alpha.V = diag(1000,2,2), alpha.mu = rep(0,2)),
+                                G2 = list(V = diag(2), nu = 0.002, alpha.V = diag(1000,2,2), alpha.mu = rep(0,2))))
+
+#m1
+if(m1){
+  m1 <- mclapply(1:3, function(i) {
+    MCMCglmm(z.log.co2pmin ~ z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + inverseK_prior_temp2,
+             random = ~us(1+inverseK_incb_temp):id + us(1+inverseK_incb_temp):series,
+             family = "gaussian",
+             prior = expanded.prior,
+             nitt = 5010000,
+             burnin = 10000,
+             thin = 5000,
+             data = dat, 
+             verbose = T)
+  }, mc.cores = 3)
+}else{
 m1 <- readRDS("output/rds/m1")
+}
 
 m1.S <- lapply(m1, function(m) m$Sol)
-m1.Sol <- do.call(mcmc.list, m1.S)
+m1.Sol <- do.call(rbind, m1.S)
 
 m1.V <- lapply(m1, function(m) m$VCV)
-m1.VCV <- do.call(mcmc.list, m1.V)
+m1.VCV <- do.call(rbind, m1.V)
 
 gelman.diag(m1.Sol)
 summary(m1.Sol)
@@ -24,24 +50,44 @@ summary(m1.VCV)
 posterior.mode(m1.VCV)
 HPDinterval(as.mcmc(rbind(m1.VCV[[1]], m1.VCV[[2]], m1.VCV[[3]])))
 
-#Tabulating the model output
+#Calculating repeatabilities using m2
+#m2
+if(m2){
+  m2 <- mclapply(1:3, function(i) {
+    MCMCglmm(z.log.co2pmin ~ z.log.co2pmin ~ inverseK_incb_temp + z.log.mass + inverseK_prior_temp2,
+             random = ~us(1+inverseK_incb_temp):id + us(1+inverseK_incb_temp):series,
+             family = "gaussian",
+             prior = expanded.prior,
+             nitt = 7510000,
+             burnin = 10000,
+             thin = 5000,
+             data = dat, 
+             verbose = T)
+  }, mc.cores = 3)
+}else{
+  m2 <- readRDS("output/rds/m2")
+}
 
+m2.S <- lapply(m2, function(m) m$Sol)
+m2.S1 <- do.call(rbind, m2.S)
+
+#Tabulating the model output using m2
 Table1 <- data.frame(matrix(nrow = 15 , ncol = 3))
 #rownames(Table1) <- c("Interpcet", "inverseK_incb_temp", "z.log.mass", "inverseK_prior_temp2",
 #                      "IDintercept", "IDslope", "COV IDintercept-IDslope",
 #                      "Sintercept", "Sslope", "COV Sintercept-Sslope",
 #                      "e", "Rint", "Rslope", "Rshort", "Rlong")
 
-rownames(Table1) <- c(names(c(posterior.mode(m1.Sol), posterior.mode(m1.VCV)[c(1,4,2,5,8,6,9)])), "Rint", "Rslope", "Rshort", "Rlong")
+rownames(Table1) <- c(names(c(posterior.mode(m2.Sol), posterior.mode(m2.VCV)[c(1,4,2,5,8,6,9)])), "Rint", "Rslope", "Rshort", "Rlong")
 colnames(Table1) <- c("estimate", "lower", "upper")
 
 #Tabulating fixed efs and CIs
-Table1[1:4, 1] <- posterior.mode(m1.Sol)
-Table1[1:4, 2:3] <- HPDinterval(as.mcmc(rbind(m1.Sol[[1]], m1.Sol[[2]], m1.Sol[[3]])))
+Table1[1:4, 1] <- posterior.mode(m2.Sol)
+Table1[1:4, 2:3] <- HPDinterval(as.mcmc(rbind(m2.Sol[[1]], m2.Sol[[2]], m2.Sol[[3]])))
 
 #Tabulating ran efs and CIs
-Table1[5:11, 1] <- posterior.mode(m1.VCV)[c(1,4,2,5,8,6,9)]
-Table1[5:11, 2:3] <- HPDinterval(as.mcmc(rbind(m1.VCV[[1]], m1.VCV[[2]], m1.VCV[[3]])))[c(1,4,2,5,8,6,9),]
+Table1[5:11, 1] <- posterior.mode(m2.VCV)[c(1,4,2,5,8,6,9)]
+Table1[5:11, 2:3] <- HPDinterval(as.mcmc(rbind(m2.VCV[[1]], m2.VCV[[2]], m2.VCV[[3]])))[c(1,4,2,5,8,6,9),]
 
 #Tabulating repeatabilities and CIs
 #Rint
@@ -63,15 +109,17 @@ Table1[15,2:3] <- Table1[5,2:3] / (Table1[5,2:3] + Table1[8,2:3] + Table1[11,2:3
 write.csv(Table1, "output/table/Table1.csv")
 write.csv(round(Table1,2), "output/table/Table1_rounded.csv")
 
-#trying to make predictions
+#Individual predictions from m2 for plots
 
-#source("R/MCMCchains_function.R")
+post <- m2.S1
 
-#m2 <- MCMC.chains("output/rds/m2")
+extractID <- function(post, fixef = colnames(post)[1:4], id = "ld0133"){
+    columns <- as.character(c(fixef, colnames(post)[grep(id, colnames(post))]))  
+    solID <- post[,columns]
+    return(solID)
+}
+test <- extractID(m2.S1)
 
-m2 <- readRDS("output/rds/m2")
-
-m2.S <- lapply(m2, function(m) m$Sol)
 m2.Sol <- do.call(mcmc.list, m2.S)
 
 plot(m2.Sol)
@@ -89,24 +137,21 @@ summary(m2.VCV)
 posterior.mode(m2.VCV)
 HPDinterval(as.mcmc(rbind(m2.VCV[[1]], m2.VCV[[2]], m2.VCV[[3]])))
 
+#Intercept model for calculating PCV 
+#m3
+if(m3){
+  m3 <- mclapply(1:3, function(i) {
+    MCMCglmm(z.log.co2pmin ~ 1,
+             random = ~us(1+inverseK_incb_temp):id + us(1+inverseK_incb_temp):series,
+             family = "gaussian",
+             prior = expanded.prior,
+             nitt = 7510000,
+             burnin = 10000,
+             thin = 5000,
+             data = dat, 
+             verbose = T)
+  }, mc.cores = 3)
+}else{
+  m3 <- readRDS("output/rds/m3")
+}
 
-str(m2)
-str(m2$solVCVlist)
-names(data) 
-
-newdata = data.frame(inverseK_incb_temp = rep(c(-0.6547592,  0.6333022, -0.1292009,  0.1283428, -0.3902114,  0.3824882),42*10),
-                     z.log.mass = rep(0, 420*6),
-                     inverseK_prior_temp2 = rep(-0.001947, 420*6),
-                     id = rep(unique(data$id), 10*6),
-                     series = rep(seq(1:10), each = 42*6))
-
-X <-model.matrix(~inverseK_incb_temp + z.log.mass + inverseK_prior_temp2 , data=newdata)
-
-beta <- m1$solVCVchain$Sol[,1]
-
-newdata$pred_zco2pm <- X %*% beta
-newdata$pred_logco2pm <-(newdata$pred_zco2pm * sd(newdata$pred_zco2pm)) + mean(newdata$pred_zco2pm)
-newdata$pred_co2pm <- exp(newdata$pred_logco2pm)
-
-
-predict.MCMCglmm(m1, newdata = newdata)
