86fd856131e0c0e86d72e979e37635892ed64bab
Fonti Kar <fonti.kar@gmail.com>
2017-09-01 14:30:15 +1000

    Cleaned code for katana

--- R/multivar_MCMC.R
+++ R/multivar_MCMC.R
@@ -6,30 +6,30 @@ rm(list = ls())
 #load library
 library(MCMCglmm)
 library(parallel)
-library(dplyr)
 
 #data
-data <- read.csv("data/data_final/mrrxn_final_v2.csv")
-data$id <- as.factor(data$id)
-data$series <- as.factor(data$series)
+data <- read.csv("data/data_final/multiresp.csv")
+#data$id <- as.factor(data$id)
+#data$series <- as.factor(data$series)
 
-varibs.need <- c("obs", "samp_period", "id" ,"batch", "series", "incb_num", "incb_temp_id", "defecate", "incb_temp", "z.incb_temp", "z.log.temp", "incb_temp_K", "inverseK_incb_temp", "body_temp", "z.body_temp", "z.log.body_temp", "body_temp_K", "inverseK_body_temp" , "z.prior_temp1", "z.log.prior_temp1", "prior_temp1_K", "inverseK_prior_temp1", "prior_temp2_K", "inverseK_prior_temp2", "z.prior_temp2", "z.log.prior_temp2", "orig_lizmass", "lizmass_nocombout","orig_lizmass", "log.mass", "z.log.mass","orig_co2_pmin", "co2pm_nocombout", "log.co2pmin", "z.log.co2pmin")
+#varibs.need <- c("obs", "samp_period", "id" ,"batch", "series", "incb_num", "incb_temp_id", "defecate", "incb_temp", "z.incb_temp", "z.log.temp", "incb_temp_K", "inverseK_incb_temp", "body_temp", "z.body_temp", "z.log.body_temp", "body_temp_K", "inverseK_body_temp" , "z.prior_temp1", "z.log.prior_temp1", "prior_temp1_K", "inverseK_prior_temp1", "prior_temp2_K", "inverseK_prior_temp2", "z.prior_temp2", "z.log.prior_temp2", "orig_lizmass", "lizmass_nocombout","orig_lizmass", "log.mass", "z.log.mass","orig_co2_pmin", "co2pm_nocombout", "log.co2pmin", "z.log.co2pmin")
 
-incl.vars <- names(data) %in% varibs.need
-data <- data[incl.vars]
+#incl.vars <- names(data) %in% varibs.need
+#data <- data[incl.vars]
 
-data3 <- data %>% 
-  select(id, samp_period, series, incb_temp, z.log.co2pmin, orig_lizmass) %>%
-  group_by(series) %>%
-  mutate(avg_mass = mean(orig_lizmass)) %>%
-  select(id, samp_period, series, incb_temp, z.log.co2pmin, avg_mass) %>%
-  spread(incb_temp, z.log.co2pmin) 
+#data3 <- data %>% 
+  #select(id, samp_period, series, incb_temp, z.log.co2pmin, orig_lizmass) %>%
+  #group_by(series) %>%
+  #mutate(avg_mass = mean(orig_lizmass)) %>%
+  #select(id, samp_period, series, incb_temp, z.log.co2pmin, avg_mass) %>%
+  #spread(incb_temp, z.log.co2pmin) 
   
-  colnames(data3)[5:10] <- paste0("t_", colnames(data3)[5:10])
+# colnames(data3)[5:10] <- paste0("t_", colnames(data3)[5:10])
 
-data3$z.log.mass <- scale(log(data3$avg_mass))
-data3 <- as.data.frame(data3)
-data4 <- data3[!is.na(data3$z.log.mass),]
+#data3$z.log.mass <- scale(log(data3$avg_mass))
+#data3 <- as.data.frame(data3)
+#data4 <- data3[!is.na(data3$z.log.mass),]
+#(data4, row.names = F, "data/data_final/multiresp.csv")
 
 #priors
 multi.prior <- list(R = list(V = diag(6), nu = 0.01), G = list(G1 = list(V = diag(6), nu = 0.01)))
