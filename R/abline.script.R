b511c8438a1dcbff31671d84ac2aaf32b45b9639
Fonti Kar <fonti.kar@gmail.com>
2017-11-04 22:03:27 +1100

    script to calculation temp specific fv apt, and linear reg lines for within individual reaction norms

---
+++ R/abline.script.R
@@ -0,0 +1,130 @@
+#Script to get indivual intercept and slope + their global intercept and slope and then plotting this as abline lines
+#What do I need for this....
+#Dataframe for each individual at each sampling period at each temp
+#lizard.id, sampling.period, temp, pred.mr, intercept, slope
+#so the intercept will be global.intercept at time + id.intercept blup at the time
+#and the slope will be the global.slope + id.slope at the time - but I don't need a temp!? 
+
+
+ab.predictions <- function(id = "ld0133", post, sampling.period=1){
+  
+  # Check that this combo is in the dataset
+  focal.series <- paste(id, sampling.period, sep = "_")
+  if(!(focal.series %in% dat$series)) return(NULL)
+  print(focal.series)
+  
+  #setting up lizard and sampling period variables use for calculating predictions
+  lizard.id <- paste("(Intercept).id.", id, sep = "")
+  
+  sampling.id <- paste("(Intercept).series.", id, "_", sampling.period, sep = "") # one can pick the sampling period, e.g. 1, or 2...10
+  
+  lizard.slope <- paste("inverseK_incb_temp.id.", id, sep = "")
+  sampling.slope <- paste("inverseK_incb_temp.series.", id, "_", sampling.period, sep = "")
+  
+  post <- post %>% as.data.frame()
+  
+  #calculating predictions for my measured temperatures
+  out <- do.call("cbind", lapply(1:length(incubation.temperatures), function(i){
+    focal.answer <- post[, names(post) == "(Intercept)"] +            # global intercept
+      post[, names(post) == lizard.id] + post[, names(post) == sampling.id] + # lizard- and sampling period-specific random intercepts
+      (incubation.temperatures[i] * (post$inverseK_incb_temp + post[, names(post) == lizard.slope] + post[, names(post) == sampling.slope])) + # temp * (global.slope +  slope for THIS lizard in THIS sampling period)
+      (post$inverseK_prior_temp2 * mean.prior.temp) # fixed effect of prior temp, assuming mean value of prior temp
+    # we leave out mass, since average is zero, so we are calcualting this for an average mass liard
+  }))
+  
+  #compiling predictions into data frame
+  data.frame(pred.mr = c(out),
+             Temperature = rep(incubation.temperatures, each = nrow(out)),
+             Lizard = id,
+             liz.intercept = post[, names(post) == "(Intercept)"] + post[, names(post) == lizard.id] + post[, names(post) == sampling.id] + (post$inverseK_prior_temp2 * mean.prior.temp),
+             liz.slope = post$inverseK_incb_temp + post[, names(post) == lizard.slope] + post[, names(post) == sampling.slope],
+             stringsAsFactors = FALSE)
+}
+
+# We will calculate the reaction norms assuming that all lizards had the mean value for 'prior temp'
+mean.prior.temp <- mean(dat$inverseK_prior_temp2, na.rm = T)
+
+# Here are the six incubator temperatures, 
+incubation.temperatures <- unique(dat$inverseK_incb_temp)
+
+#and the 42 lizard names
+lizard.names <- data$id %>% as.character %>% unique %>% sort
+
+m2 <- readRDS("output/rds/m2")
+
+m2.S <- lapply(m2, function(m) m$Sol)
+m2.Sol <- do.call(rbind, m2.S)
+m2.S1 <- do.call(mcmc.list, m2.S)
+
+m2.V <- lapply(m2, function(m) m$VCV)
+m2.V1 <- do.call(mcmc.list, m2.V)
+m2.VCV <- do.call(rbind, m2.V)
+
+ab.output <- do.call("rbind", lapply(1:10, function(i){
+  do.call("rbind", lapply(lizard.names, ab.predictions, post=cbind(m2.Sol, m2.VCV), sampling.period = i)) %>% mutate(sampling.period = i) %>% arrange(Temperature, pred.mr) %>% mutate(Lizard = factor(Lizard, levels = unique(Lizard)))
+  }))
+
+ab.reaction.norms <- ab.output %>% group_by(Temperature, Lizard, sampling.period) %>% summarise(pred.mr = posterior.mode(as.mcmc(pred.mr)), liz.intercept = posterior.mode(as.mcmc(liz.intercept)), liz.slope = posterior.mode(as.mcmc(liz.slope))) %>% as.data.frame
+ab.reaction.norms$Temperature <- inverseK_to_C(ab.reaction.norms$Temperature)
+
+str(ab.reaction.norms) ; head(ab.reaction.norms)
+
+ab.rn <- select(ab.reaction.norms, Temperature, Lizard, sampling.period, pred.mr) %>% mutate(series = paste0(Lizard, "_", sampling.period)) %>% arrange(series)
+
+lm_extract <- function(x){
+    tmpmod <- lm(pred.mr ~ Temperature, data = x)
+    coefs <-coef(tmpmod)
+  return(coefs)
+}
+
+# split
+  splitABRN <- split(ab.rn, ab.rn$series)
+
+# apply
+  ab.rn.coefs <- do.call(rbind, lapply(splitABRN, function(x) lm_extract(x)))
+  ab.rn.coefs <- as.tbl(ab.rn.coefs)
+  str(ab.rn.coefs)
+  ab.rn.coefs$series <- rownames(ab.rn.coefs)
+  ab.rn <- as.tbl(ab.rn)
+  str(ab.rn)
+  ab.rn <-left_join(ab.rn, ab.rn.coefs, by = "series")
+  names(ab.rn)[1] <- "Temperature"
+  names(ab.rn)[6] <- "b0"
+  names(ab.rn)[7] <- "b1"
+
+#pdf("output/fig/ab_reaction.norms.pdf", 10, 6)
+ab.rn %>% 
+  ggplot(aes(x = Temperature, y = pred.mr, group = Lizard)) + 
+  geom_point(shape = 1, fill = "white", size = 1, color = "black") + 
+  geom_abline(aes(intercept = b0, slope = b1, color = Lizard), alpha = 0.6) + 
+  scale_x_continuous(breaks = c(22, 24, 26, 28, 30, 32)) + 
+  facet_wrap(~ sampling.period, nrow = 2) + 
+  theme_bw() + 
+  theme(legend.position = "none", 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  labs(x = expression(paste("Temperature ",degree,"C")), y = expression(Metabolic~rate~(CO[2]~min^{-1})))  
+#dev.off()
+
+#Careau code
+plot(Phenotype.1~Phenotype.2,data=DATA,col=DATA$Individual,pch=1, xaxt="n",xlab="",ylab="",ylim=yrange,xlim=xrange)
+for(i in unique(DATA$ID)){
+  hold<-subset(DATA, ID==i)
+  model<-lm(Phenotype.1~Phenotype.2,data=hold)
+  clip(min(hold$Phenotype.2),max(hold$Phenotype.2),min(hold$Phenotype.1),max(hold$Phenotype.1))
+  abline(model, col=i)
+}
+clip(min(DATA$Phenotype.2),max(DATA$Phenotype.2),min(DATA$Phenotype.1),max(DATA$Phenotype.1))
+points(DATA.AVG$avg1~DATA.AVG$avg2, pch=16)
+clip(min(DATA.AVG$avg2),max(DATA.AVG$avg2),min(DATA.AVG$avg1),max(DATA.AVG$avg1))
+abline(lm(DATA.AVG$avg1~DATA.AVG$avg2), col="black",lwd=2)
+mtext(expression(bold("A")),side=3, line=-1.5,adj=0.025)
+mtext(expression(paste(italic("N")[ID], " = 100")), side=1,line=-1,adj=1)
+mtext(expression(paste(italic("n")[trial], " = 20")), side=1,line=-2,adj=1)
+
+
+
+
+
+
+
--- R/temp_centered models.R
+++ R/temp_centered models.R
@@ -11,6 +11,33 @@ library(ggplot2)
 
 source("R/functions/smr_functions.R")
 
+#####Setting up tables for data######
+TableRint <- data.frame(matrix(nrow = 6 , ncol = 9))
+rownames(TableRint) <- c(22, 24, 26, 28, 30, 32)
+colnames(TableRint) <- c("R_int", "R_lower", "R_upper",
+                         "ID_var", "ID_lower", "ID_upper",
+                         "Series_var", "Series_lower", "Series_upper")
+TableRint$Temperature <- rownames(TableRint)
+
+TableRslope <- data.frame(matrix(nrow = 6 , ncol = 9))
+rownames(TableRslope) <- c(22, 24, 26, 28, 30, 32)
+colnames(TableRslope) <- c("R_slope", "R_lower", "R_upper",
+                           "ID_var", "ID_lower", "ID_upper",
+                           "Series_var", "Series_lower", "Series_upper")
+TableRslope$Temperature <- rownames(TableRslope)
+
+TableRshort <- data.frame(matrix(nrow = 6 , ncol = 3))
+rownames(TableRshort) <- c(22, 24, 26, 28, 30, 32)
+colnames(TableRshort) <- c("Repeatability", "lower", "upper")
+
+TableRlong <- data.frame(matrix(nrow = 6 , ncol = 3))
+rownames(TableRlong) <- c(22, 24, 26, 28, 30, 32)
+colnames(TableRlong) <- c("Repeatability", "lower", "upper")
+
+TableIntSlopeCovCor <- data.frame(matrix(nrow = 6 , ncol = 6))
+rownames(TableIntSlopeCovCor) <- c(22, 24, 26, 28, 30, 32)
+colnames(TableIntSlopeCovCor) <- c("Covariance", "Cov_lower", "Cov_upper", "Correlation", "Cor_lower", "Cor_upper")
+
 ####################################
 #### INTERCEPT at 22 degrees C #####
 ####################################
@@ -39,14 +66,30 @@ posterior.mode(m1_t22.VCV) ; HPDinterval(as.mcmc(m1_t22.VCV))
 ##Rint
 t22_R.int <- m1_t22.VCV[,"(Intercept):(Intercept).id"] / ( m1_t22.VCV[,"(Intercept):(Intercept).id"] + m1_t22.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t22_R.int) #R.int
-HPDinterval(as.mcmc(t22_R.int)) #R.int CIs
+TableRint[1,1] <- posterior.mode(t22_R.int) #R.int
+TableRint[1,2:3] <-HPDinterval(as.mcmc(t22_R.int)) #R.int CIs
+
+#Rint Between ID variance
+TableRint[1,4] <- posterior.mode(m1_t22.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[1,5:6] <- HPDinterval(as.mcmc(m1_t22.VCV[,"(Intercept):(Intercept).id"]))
+
+#Rint Series ID (Within ID - amoung series) variance
+TableRint[1,7] <- posterior.mode(m1_t22.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[1,8:9] <- HPDinterval(as.mcmc(m1_t22.VCV[,"(Intercept):(Intercept).series"]))
 
 ##Rslope
 t22_R.slope <- m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.id"] / ( m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.id"] + m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.series"] ) 
 
-posterior.mode(t22_R.slope) #Rslope
-HPDinterval(as.mcmc(t22_R.slope)) #Rslope CIs
+TableRslope[1,1] <- posterior.mode(t22_R.slope) #Rslope
+TableRslope[1,2:3] <- HPDinterval(as.mcmc(t22_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[1,4] <- posterior.mode(m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.id"])
+TableRslope[1,5:6] <- HPDinterval(as.mcmc(m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[1,7] <- posterior.mode(m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.series"])
+TableRslope[1,8:9] <- HPDinterval(as.mcmc(m1_t22.VCV[,"inverseK_incb_temp_22cen:inverseK_incb_temp_22cen.series"]))
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -88,14 +131,30 @@ posterior.mode(m1_t24.VCV) ; HPDinterval(as.mcmc(m1_t24.VCV))
 ##Rint
 t24_R.int <- m1_t24.VCV[,"(Intercept):(Intercept).id"] / ( m1_t24.VCV[,"(Intercept):(Intercept).id"] + m1_t24.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t24_R.int) #R.int
-HPDinterval(as.mcmc(t24_R.int)) #R.int CIs
+TableRint[2,1] <- posterior.mode(t24_R.int) #R.int
+TableRint[2,2:3] <-HPDinterval(as.mcmc(t24_R.int)) #R.int CIs
+
+#Between ID variance
+TableRint[2,4] <- posterior.mode(m1_t24.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[2,5:6] <- HPDinterval(as.mcmc(m1_t24.VCV[,"(Intercept):(Intercept).id"]))
+
+#Series ID (Within ID - amoung series) variance
+TableRint[2,7] <- posterior.mode(m1_t24.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[2,8:9] <- HPDinterval(as.mcmc(m1_t24.VCV[,"(Intercept):(Intercept).series"]))
 
 ##Rslope
 t24_R.slope <- m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.id"] / ( m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.id"] + m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.series"] ) 
 
-posterior.mode(t24_R.slope) #Rslope
-HPDinterval(as.mcmc(t24_R.slope)) #Rslope CIs
+TableRslope[2,1] <- posterior.mode(t24_R.slope) #Rslope
+TableRslope[2,2:3] <- HPDinterval(as.mcmc(t24_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[2,4] <- posterior.mode(m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.id"])
+TableRslope[2,5:6] <- HPDinterval(as.mcmc(m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[2,7] <- posterior.mode(m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.series"])
+TableRslope[2,8:9] <- HPDinterval(as.mcmc(m1_t24.VCV[,"inverseK_incb_temp_24cen:inverseK_incb_temp_24cen.series"]))
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -137,14 +196,30 @@ posterior.mode(m1_t26.VCV) ; HPDinterval(as.mcmc(m1_t26.VCV))
 ##Rint
 t26_R.int <- m1_t26.VCV[,"(Intercept):(Intercept).id"] / ( m1_t26.VCV[,"(Intercept):(Intercept).id"] + m1_t26.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t26_R.int) #R.int
-HPDinterval(as.mcmc(t26_R.int)) #R.int CIs
+TableRint[3,1] <- posterior.mode(t26_R.int)  #R.int
+TableRint[3,2:3] <-HPDinterval(as.mcmc(t26_R.int)) #R.int CIs
+
+#Between ID variance
+TableRint[3,4] <- posterior.mode(m1_t26.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[3,5:6] <- HPDinterval(as.mcmc(m1_t26.VCV[,"(Intercept):(Intercept).id"]))
+
+#Series ID (Within ID - amoung series) variance
+TableRint[3,7] <- posterior.mode(m1_t26.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[3,8:9] <- HPDinterval(as.mcmc(m1_t26.VCV[,"(Intercept):(Intercept).series"]))
 
 ##Rslope
 t26_R.slope <- m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.id"] / ( m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.id"] + m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.series"] ) 
 
-posterior.mode(t26_R.slope) #Rslope
-HPDinterval(as.mcmc(t26_R.slope)) #Rslope CIs
+TableRslope[3,1] <- posterior.mode(t26_R.slope) #Rslope
+TableRslope[3,2:3] <- HPDinterval(as.mcmc(t26_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[3,4] <- posterior.mode(m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.id"])
+TableRslope[3,5:6] <- HPDinterval(as.mcmc(m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[3,7] <- posterior.mode(m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.series"])
+TableRslope[3,8:9] <- HPDinterval(as.mcmc(m1_t26.VCV[,"inverseK_incb_temp_26cen:inverseK_incb_temp_26cen.series"]))
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -186,14 +261,30 @@ posterior.mode(m1_t28.VCV) ; HPDinterval(as.mcmc(m1_t28.VCV))
 ##Rint
 t28_R.int <- m1_t28.VCV[,"(Intercept):(Intercept).id"] / ( m1_t28.VCV[,"(Intercept):(Intercept).id"] + m1_t28.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t28_R.int) #R.int
-HPDinterval(as.mcmc(t28_R.int)) #R.int CIs
+TableRint[4,1] <- posterior.mode(t28_R.int) #R.int
+TableRint[4,2:3] <- HPDinterval(as.mcmc(t28_R.int)) #R.int CIs
+
+#Between ID variance
+TableRint[4,4] <- posterior.mode(m1_t28.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[4,5:6] <- HPDinterval(as.mcmc(m1_t28.VCV[,"(Intercept):(Intercept).id"]))
+
+#Series ID (Within ID - amoung series) variance
+TableRint[4,7] <- posterior.mode(m1_t28.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[4,8:9] <- HPDinterval(as.mcmc(m1_t28.VCV[,"(Intercept):(Intercept).series"]))
 
 ##Rslope
 t28_R.slope <- m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.id"] / ( m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.id"] + m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.series"] ) 
 
-posterior.mode(t28_R.slope) #Rslope
-HPDinterval(as.mcmc(t28_R.slope)) #Rslope CIs
+TableRslope[4,1] <- posterior.mode(t28_R.slope) #Rslope
+TableRslope[4,2:3] <- HPDinterval(as.mcmc(t28_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[4,4] <- posterior.mode(m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.id"])
+TableRslope[4,5:6] <- HPDinterval(as.mcmc(m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[4,7] <- posterior.mode(m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.series"])
+TableRslope[4,8:9] <- HPDinterval(as.mcmc(m1_t28.VCV[,"inverseK_incb_temp_28cen:inverseK_incb_temp_28cen.series"]))
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -235,14 +326,30 @@ posterior.mode(m1_t30.VCV) ; HPDinterval(as.mcmc(m1_t30.VCV))
 ##Rint
 t30_R.int <- m1_t30.VCV[,"(Intercept):(Intercept).id"] / ( m1_t30.VCV[,"(Intercept):(Intercept).id"] + m1_t30.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t30_R.int) #R.int
-HPDinterval(as.mcmc(t30_R.int)) #R.int CIs
+TableRint[5,1] <- posterior.mode(t30_R.int) #R.int
+TableRint[5,2:3] <- HPDinterval(as.mcmc(t30_R.int)) #R.int CIs
+
+#Between ID variance
+TableRint[5,4] <- posterior.mode(m1_t30.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[5,5:6] <- HPDinterval(as.mcmc(m1_t30.VCV[,"(Intercept):(Intercept).id"]))
+
+#Series ID (Within ID - amoung series) variance
+TableRint[5,7] <- posterior.mode(m1_t30.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[5,8:9] <- HPDinterval(as.mcmc(m1_t30.VCV[,"(Intercept):(Intercept).series"]))
 
 ##Rslope
 t30_R.slope <- m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.id"] / ( m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.id"] + m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.series"] ) 
 
-posterior.mode(t30_R.slope) #Rslope
-HPDinterval(as.mcmc(t30_R.slope)) #Rslope CIs
+TableRslope[5,1] <- posterior.mode(t30_R.slope) #Rslope
+TableRslope[5,2:3] <- HPDinterval(as.mcmc(t30_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[5,4] <- posterior.mode(m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.id"])
+TableRslope[5,5:6] <- HPDinterval(as.mcmc(m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[5,7] <- posterior.mode(m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.series"])
+TableRslope[5,8:9] <- HPDinterval(as.mcmc(m1_t30.VCV[,"inverseK_incb_temp_30cen:inverseK_incb_temp_30cen.series"]))
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -284,14 +391,36 @@ posterior.mode(m1_t32.VCV) ; HPDinterval(as.mcmc(m1_t32.VCV))
 ##Rint
 t32_R.int <- m1_t32.VCV[,"(Intercept):(Intercept).id"] / ( m1_t32.VCV[,"(Intercept):(Intercept).id"] + m1_t32.VCV[,"(Intercept):(Intercept).series"] ) 
 
-posterior.mode(t32_R.int) #R.int
-HPDinterval(as.mcmc(t32_R.int)) #R.int CIs
+TableRint[6,1] <- posterior.mode(t32_R.int) #R.int
+TableRint[6,2:3] <- HPDinterval(as.mcmc(t32_R.int)) #R.int CIs
+
+#Between ID variance
+TableRint[6,4] <- posterior.mode(m1_t32.VCV[,"(Intercept):(Intercept).id"]) 
+TableRint[6,5:6] <- HPDinterval(as.mcmc(m1_t32.VCV[,"(Intercept):(Intercept).id"]))
+
+#Series ID (Within ID - amoung series) variance
+TableRint[6,7] <- posterior.mode(m1_t32.VCV[,"(Intercept):(Intercept).series"]) 
+TableRint[6,8:9] <- HPDinterval(as.mcmc(m1_t32.VCV[,"(Intercept):(Intercept).series"]))
+
+round(TableRint[1:8],2)
+
+write.csv(round(TableRint[1:8],2), row.names = F, "output/table/FV_Rint_varcomp.csv")
 
 ##Rslope
 t32_R.slope <- m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.id"] / ( m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.id"] + m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.series"] ) 
 
-posterior.mode(t32_R.slope) #Rslope
-HPDinterval(as.mcmc(t32_R.slope)) #Rslope CIs
+TableRslope[6,1] <- posterior.mode(t32_R.slope) #Rslope
+TableRslope[6,2:3] <- HPDinterval(as.mcmc(t32_R.slope)) #Rslope CIs
+
+#Rslope between ID variance
+TableRslope[6,4] <- posterior.mode(m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.id"])
+TableRslope[6,5:6] <- HPDinterval(as.mcmc(m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.id"]))
+
+#Rslope within ID variance amoung series variance
+TableRslope[6,7] <- posterior.mode(m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.series"])
+TableRslope[6,8:9] <- HPDinterval(as.mcmc(m1_t32.VCV[,"inverseK_incb_temp_32cen:inverseK_incb_temp_32cen.series"]))
+
+write.csv(round(TableRslope[1:8],2), row.names = F, "output/table/FV_Rslope_varcomp.csv")
 
 ##Covariance/Correlation of intercept and slope ID level
 
@@ -305,5 +434,85 @@ m1_t32.series.cor <- m1_t32.VCV[,"inverseK_incb_temp_32cen:(Intercept).series"] 
 posterior.mode(m1_t32.series.cor)
 HPDinterval(as.mcmc(m1_t32.series.cor))
 
+posterior.mode(m1_t32.series.cor) #R.int
+HPDinterval(as.mcmc(m1_t32.series.cor)) #R.int CIs
 
+#Plotting FV forest plots - Repeatability in Intercepts
+Rint_FV1 <- ggplot(data = TableRint, aes(x = Temperature, y = Repeatability)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = R_lower, ymax = R_upper), width = 0) +
+  scale_y_continuous(limits = c(0,1)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Repeatability", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
 
+#Plotting FV forest plots - Between ID variance in intercept
+Rint_FV2 <- ggplot(data = TableRint, aes(x = Temperature, y = ID_var)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ID_lower, ymax = ID_upper), width = 0) +
+  scale_y_continuous(limits = c(0,0.4)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Between-individual variance", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+#Plotting FV forest plots - Within ID - amoung series variance in intercept
+Rint_FV3 <- ggplot(data = TableRint, aes(x = Temperature, y = Series_var)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = Series_lower, ymax = Series_upper), width = 0) +
+  scale_y_continuous(limits = c(0,0.4)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Within-individual-between-series\n variance", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+#pdf("output/fig/FVvariancecomponentts.pdf", 12, 7)
+multiplot(Rint_FV1, Rint_FV2, Rint_FV3, cols = 3)
+#dev.off()
+
+#Plotting FV forest plots - Repeatability in Slope
+Rslope_FV1 <- ggplot(data = TableRslope, aes(x = Temperature, y = R_slope)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = R_lower, ymax = R_upper), width = 0) +
+  scale_y_continuous(limits = c(0,1)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Repeatability", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+#Plotting FV forest plots - Between ID variance in intercept
+Rslope_FV2 <- ggplot(data = TableRslope, aes(x = Temperature, y = ID_var)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = ID_lower, ymax = ID_upper), width = 0) +
+  scale_y_continuous(limits = c(0,0.2)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Between-individual variance", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+#Plotting FV forest plots - Within ID - amoung series variance in intercept
+Rslope_FV3 <- ggplot(data = TableRslope, aes(x = Temperature, y = Series_var)) +
+  geom_point() + 
+  geom_errorbar(aes(ymin = Series_lower, ymax = Series_upper), width = 0) +
+  scale_y_continuous(limits = c(0,0.2)) + 
+  #scale_x_continuous(breaks = x) + 
+  labs(y = "Within-individual-between-series\n variance", x = "Temperature") +
+  coord_flip() +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+#pdf("output/fig/FV_Rslope_variancecomponentts.pdf", 12, 7)
+multiplot(Rslope_FV1, Rslope_FV2, Rslope_FV3, cols = 3)
+#dev.off()
