856fb48c5ccbaa1829148f096e77a253c00dedb1
Fonti Kar <fonti.kar@gmail.com>
2017-11-14 20:48:12 +1100

    Code to make AES figures

--- R/mrrxn_MCMC.R
+++ R/mrrxn_MCMC.R
@@ -257,11 +257,11 @@ reaction.norms %>%
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) + 
   labs(x = expression(paste("Temperature ",degree,"C")), y = expression(Metabolic~rate~(CO[2]~min^{-1})))
-#dev.off()
-reaction.norms %>% 
+#pdf("output/fig/AES_Fig1", 9,9)
+filter(reaction.norms, sampling.period ==5) %>% 
   ggplot(aes(x = Temperature, y = posterior.mode, group = Lizard, color = Lizard)) +
-  geom_line(aes(group = Lizard, colour = Lizard), stat="smooth", method = "lm", alpha = 0.6) +
-  geom_point(shape = 1, fill = "white", size = 1, color = "black") +
+  geom_line(aes(group = Lizard, colour = Lizard), stat="smooth", method = "lm", alpha = 0.6, lwd = 1) +
+  geom_point(shape = 1, fill = "white", size = 1, colour = "black") +
   #facet_wrap(~ sampling.period, nrow = 2) + 
   scale_x_continuous(breaks = c(22, 24, 26, 28, 30, 32)) + 
   theme_bw() + 
@@ -271,7 +271,7 @@ reaction.norms %>%
         axis.text=element_text(size=12),
         axis.title=element_text(size=14,face="bold")) + 
   labs(x = expression(paste("Temperature ",degree,"C")), y = expression(Metabolic~rate~(VCO[2]~min^{-1})))
-
+#dev.off()
 
 #Plotting covariances
 #Individual predictions from m2 for plots
