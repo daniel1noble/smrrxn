5c1320af7ea7ab0eac1a45886aaa4d1326d567d8
Fonti Kar <fonti.kar@gmail.com>
2018-01-07 09:22:17 +1100

    no if statements

--- R/katana_m1log_recenter_22.R
+++ R/katana_m1log_recenter_22.R
@@ -13,15 +13,6 @@ expanded.prior <- list(R = list(V = 1, nu = 0.002),
                        G = list(G1 = list(V = diag(2), nu = 0.002, alpha.V = diag(1000,2,2), alpha.mu = rep(0,2)),
                                 G2 = list(V = diag(2), nu = 0.002, alpha.V = diag(1000,2,2), alpha.mu = rep(0,2))))
 
-
-m1log_t22 <- TRUE
-m1log_t24 <- FALSE
-m1log_t26 <- FALSE
-m1log_t28 <- FALSE
-m1log_t30 <- FALSE
-m1log_t32 <- FALSE
-
-
 #22 centered
   m1log_t22 <- mclapply(1:3, function(i) {
     MCMCglmm(log.co2pmin ~ inverseK_incb_temp_22cen + z.log.mass,
