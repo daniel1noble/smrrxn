a25c4f4da5407a912d55e6434fca55bbfa2c7320
Fonti Kar <fonti.kar@gmail.com>
2018-02-01 08:30:23 +1100

    complete cases

--- R/logTemp/katana_m1.logMR.logT_recenter_22.R
+++ R/logTemp/katana_m1.logMR.logT_recenter_22.R
@@ -8,6 +8,10 @@ data <- read.csv("data/data_final/mr_final_log.T_recentered.csv")
 data$id <- as.factor(data$id)
 data$series <- as.factor(data$series)
 
+predictors <- c("log.temp_22cen", "z.log.mass", "id", "series")
+
+data <- data[complete.cases(data[,predictors]),]
+
 #priors
 expanded.prior <- list(R = list(V = 1, nu = 0.002),
                        G = list(G1 = list(V = diag(2), nu = 0.002, alpha.V = diag(1000,2,2), alpha.mu = rep(0,2)),
@@ -19,10 +23,10 @@ m1.logMR.logT.npT.t22 <- mclapply(1:3, function(i) {
            random = ~us(1+log.temp_22cen):id + us(1+log.temp_22cen):series,
            family = "gaussian",
            prior = expanded.prior,
-           nitt = 7510000,
-           burnin = 10000,
-           thin = 5000,
-           data = data, 
+           nitt = 7510,
+           burnin = 10,
+           thin = 5,
+           data = , 
            verbose = T)
 }, mc.cores = 3)
   
